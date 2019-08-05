d5eecbba60e99d404b27c2d53b367b472576ce04
Isaiah Gao <ygao67@jhu.edu>
2019-08-04 21:00:07 -0500

    Add ability to load legacy user list. Add error checking to legacy load.

--- src/main/java/jhunions/isaiahgao/server/model/UserHandler.java
+++ src/main/java/jhunions/isaiahgao/server/model/UserHandler.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 import org.jetbrains.annotations.Nullable;
 
+import jhunions.isaiahgao.common.FullName;
 import jhunions.isaiahgao.common.User;
 
 public class UserHandler {
@@ -19,7 +20,36 @@ public class UserHandler {
 	}
 	
 	public void load() {
+		boolean shouldSaveAll = false;
+		
+		// check for legacy
 		try {
+			File file = new File("oldusers");
+			if (!file.exists()) {
+				throw new InterruptedException();
+			}
+			
+			Scanner sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				String str = sc.nextLine();
+				String[] arr = str.split("\t");
+				
+				User user = new User(arr[0], new FullName(arr[1], arr[2]), arr[3], Long.parseLong(arr[4]));
+				if (user.checkForErrors() == null)
+					byHopkinsId.put(user.getHopkinsID(), user);
+			}
+			sc.close();
+			
+			shouldSaveAll = true;
+			file.renameTo(new File("oldusers.processed"));
+		} catch (InterruptedException e) {
+			
+		} catch (Exception e) {
+			System.err.println("Failed to load old users.");
+			e.printStackTrace();
+		}
+		
+ 		try {
 			File file = new File("users");
 			if (!file.exists()) {
 				file.createNewFile();
@@ -44,6 +74,10 @@ public class UserHandler {
 			System.err.println("Could not load users.");
 			System.exit(1);
 		}
+ 		
+ 		if (shouldSaveAll) {
+ 			this.saveAll();
+ 		}
 	}
 	
 	public @Nullable User getById(String id) {
@@ -56,6 +90,23 @@ public class UserHandler {
 	
 	public void registerUser(User user) {
 		this.byHopkinsId.put(user.getHopkinsID(), user);
+		append(user);
+	}
+	
+	public void saveAll() {
+		try {
+			File file = new File("users");
+			FileWriter writer = new FileWriter(file);
+			for (User user : this.byHopkinsId.values()) {
+				writer.write(user.toString() + System.lineSeparator());
+			}
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void append(User user) {
 		// save
 		try {
 			File file = new File("users");
